4791444375e4f5307cce136fb0e8b5c61f5cbe26
Nilson <njsouza.cfg@gmail.com>
2022-10-21 13:52:32 -0300

    Meus Exercícios de Map

---
+++ 01 - Bootcamp-Quebec-Java-Digital/02 - Dominando a Linguagem de Programação Java/07 - Trabalhando com Collections Java/04 - Maps - Exemplos Professora Camila/MeusTestes.java
@@ -0,0 +1,60 @@
+package br.com.dio.collection.map;
+
+import java.util.*;
+
+public class MeusTestes {
+    public static void main(String[] args) {
+        Map<String, Double> carrosPopulares = new HashMap<>() {{
+            put("gol", 14.4);
+            put("uno", 15.6);
+            put("mobi", 16.1);
+            put("hb20", 14.5);
+            put("kwid", 15.6);
+        }};
+        System.out.println(carrosPopulares.toString());
+
+        System.out.println("Substitua o consumo do gol por 15,2 km/l: ");
+        carrosPopulares.put("gol", 15.2);
+        System.out.println(carrosPopulares);
+
+        System.out.println("Confira se o modelo tucson está no dicionário: " + carrosPopulares.containsKey("tucson"));
+
+        System.out.println("Exiba o consumo do uno: " + carrosPopulares.get("uno"));
+
+//        System.out.println("Exiba o terceiro modelo adicionado: ");
+
+        System.out.println("Exiba os modelos: ");
+        Set<String> modelos = carrosPopulares.keySet();
+        System.out.println(modelos);
+
+        System.out.println("Exiba os consumos dos carros: ");
+        Collection<Double> consumos = carrosPopulares.values();
+        System.out.println(consumos);
+
+        System.out.println("Exiba o modelo mais econômico e seu consumo: ");
+
+        Double consumoMaisEficiente = Collections.max(carrosPopulares.values());
+        Set<Map.Entry<String, Double>> entries = carrosPopulares.entrySet();
+        String modeloMaisEficiente = "";
+
+        for (Map.Entry<String, Double> entry : entries) {
+            if (entry.getValue().equals(consumoMaisEficiente)) {
+                modeloMaisEficiente = entry.getKey();
+                System.out.println("Modelo mais eficiente: " + modeloMaisEficiente + " - " + consumoMaisEficiente);
+            }
+        }
+
+        System.out.println("Exiba o menos econömico e seu consumo");
+        String modeloMenosEficiente = "";
+
+        Double consumoMenosEficiente = Collections.min(carrosPopulares.values());
+        for (Map.Entry<String, Double> entry : carrosPopulares.entrySet()) {
+            if (entry.getValue().equals(consumoMenosEficiente)) {
+                modeloMenosEficiente = entry.getKey();
+                System.out.println("Modelo menos eficiente: " + modeloMenosEficiente + " - " + consumoMenosEficiente);
+
+            }
+        }
+
+    }
+}
